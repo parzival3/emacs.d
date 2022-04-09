376cb95eaf5d2fca96d7901b5baad4ba9cb0dbae
Enrico Tolotto <etolotto@gmail.com>
2022-04-09 09:45:02 +0200

    init: add evil and occur

--- init.el
+++ init.el
@@ -19,10 +19,125 @@
 (use-package magit
   :straight t)
 
+(use-package cider
+  :straight t)
+
 (use-package vertico
   :straight t
   :init
   (vertico-mode))
+
+(use-package evil
+  :straight t
+  :config
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-define-key 'normal 'global (kbd "<leader>SPC") 'project-find-file)
+  (evil-define-key 'normal 'global (kbd "<leader>gg") 'magit-status)
+  :init
+  (evil-mode 1))
+
+(use-package consult
+  :straight t
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings (mode-specific-map)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c k" . consult-kmacro)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ("<help> a" . consult-apropos)            ;; orig. apropos-command
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s m" . consult-multi-occur)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Optionally replace `completing-read-multiple' with an enhanced version.
+  (advice-add #'completing-read-multiple :override #'consult-completing-read-multiple)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key (kbd "M-."))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<"))
+
 
 (use-package orderless
   :straight t
@@ -56,73 +171,85 @@
   :bind (("M-/" . dabbrev-completion)
          ("C-M-/" . dabbrev-expand)))
 
-;; Configuration
-(when (eq system-type 'darwin)
-  (setq mac-command-key-is-meta   t
-	mac-command-modifier      'super
-	ns-command-modifier       'meta
-	mac-option-modifier       'meta
-	ns-option-modifier        'meta
-	mac-right-option-modifier 'none
-	ns-right-option-modifier  'none))
+(use-package emacs
+  :init
+  ;; Configuration
+  (when (eq system-type 'darwin)
+    (setq mac-command-key-is-meta   t
+	  mac-command-modifier      'super
+	  ns-command-modifier       'meta
+	  mac-option-modifier       'meta
+	  ns-option-modifier        'meta
+	  mac-right-option-modifier 'none
+	  ns-right-option-modifier  'none))
 
-;; Remove white spaces before saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+  ;; Remove white spaces before saving
+  (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; Camel-case words are separated words in program mode :-)
-(add-hook 'prog-mode-hook 'subword-mode)
+  ;; Camel-case words are separated words in program mode :-)
+  (add-hook 'prog-mode-hook 'subword-mode)
 
-;; Single space after period
-(setq sentence-end-double-space nil)
+  ;; Single space after period
+  (setq sentence-end-double-space nil)
 
-;; Delete selected text if we start typing
-(delete-selection-mode t)
+  ;; Delete selected text if we start typing
+  (delete-selection-mode t)
 
-;; Y-N as default
-(setq confirm-kill-emacs 'y-or-n-p)
-(fset 'yes-or-no-p 'y-or-n-p)
+  ;; Y-N as default
+  (setq confirm-kill-emacs 'y-or-n-p)
+  (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Silent startup
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
+  ;; Silent startup
+  (setq inhibit-startup-message t)
+  (setq initial-scratch-message nil)
 
-;; Dired file size as human redeable by default
-(setq-default dired-listing-switches "-alh")
+  ;; Dired file size as human redeable by default
+  (setq-default dired-listing-switches "-alh")
 
-;; Show matching parethesis
-(show-paren-mode t)
+  ;; Show matching parethesis
+  (show-paren-mode t)
 
-;; Don't make sound by flash screen
-(setq visible-bell t)
+  ;; Don't make sound by flash screen
+  (setq visible-bell t)
 
-;; Try to not use tabs
-(setq-default indent-tabs-mode nil)
+  ;; Try to not use tabs
+  (setq-default indent-tabs-mode nil)
 
-;; Display all the functions by default
-(setq apropos-do-all t)
+  ;; Display all the functions by default
+  (setq apropos-do-all t)
 
-;; Setup the backup directory
-(unless backup-directory-alist
+  ;; Setup the backup directory
+  (unless backup-directory-alist
     (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
                                                    "backups")))))
 
-;; Uniquify is a library to make the filename of similar buffer different
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'post-forward)
+  ;; Uniquify is a library to make the filename of similar buffer different
+  (require 'uniquify)
+  (setq uniquify-buffer-name-style 'post-forward)
 
-;; Remember the last postion visited
-(save-place-mode 1)
+  ;; Remember the last postion visited
+  (save-place-mode 1)
 
-;; UI-UX
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-(load-theme 'modus-vivendi)
+  ;; UI-UX
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1)
+  (horizontal-scroll-bar-mode -1)
+  (load-theme 'modus-vivendi)
 
-;; Prefer to load the more recent version of a file
-(setq load-prefer-newer t)
+  ;; Prefer to load the more recent version of a file
+  (setq load-prefer-newer t)
 
-;; Keybindings
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+  ;; Keybindings
+  (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+  (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+
+  ;; Set line numbers in the buffer
+  (global-display-line-numbers-mode t)
+
+  ;; Use ibuffer instead of the usual buffer menu
+  (global-set-key (kbd "C-x C-b") 'ibuffer)
+  (setq minibuffer-complete-and-exit 'after-completion)
+
+  ;; Set tab width to 4
+  (setq tab-width 4))
