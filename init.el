1fa71f8da48df19eba9109014e86f15b6266d096
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2022-05-05 12:45:32 +0200

    init: update config

--- init.el
+++ init.el
@@ -119,6 +119,7 @@
          ("M-s g" . consult-grep)
          ("M-s G" . consult-git-grep)
          ("M-s r" . consult-ripgrep)
+         ("C-s" . consult-line)
          ("M-s l" . consult-line)
          ("M-s L" . consult-line-multi)
          ("M-s m" . consult-multi-occur)
