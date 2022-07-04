c1e9b2f9f0ecd26e2bdaf72206d8961ee8644650
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2022-07-04 09:53:05 +0200

    add emacsserver bat

---
+++ scripts/emacsserver.bat
@@ -0,0 +1,3 @@
+del "C:\Users\ento\.emacs.d\.local\etc\server\server"
+
+C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -noe -c "&{Import-Module 'C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'; Enter-VsDevShell b5e65f89; C:/Tools/emacs/bin/runemacs.exe --daemon}"
