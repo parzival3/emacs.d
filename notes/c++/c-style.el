be797b9e21509e22ea225f302d78a1cdcab622ae
Enrico Tolotto <etolotto@gmail.com>
2022-08-22 11:10:31 +0200

    notes: add dci style to c++

--- notes/c++/c-style.el
+++ notes/c++/c-style.el
@@ -1,4 +1,9 @@
 (defconst dci-cc-style
  '("stroustrup" ; this is inheritance from the linux style
-   (c-offsets-alist  . ((defun-block-intro . +)
+   (c-offsets-alist  . ((top-most-intro . --)
+                        (defun-block-intro . +)
                         (func-decl-cont . 0)))))
+
+
+(c-add-style "dci" dci-cc-style)
+(setq c-default-style (append c-default-style '((cc-mode . "dci") (c++-mode . "dci") (c-mode . "dci"))))
