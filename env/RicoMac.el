0436ce915a09162bdd094b3d90a342fa37410730
Enrico Tolotto <etolotto@gmail.com>
2023-01-21 14:27:10 +0100

    evn:RicoMac: fix shell path

--- env/RicoMac.el
+++ env/RicoMac.el
@@ -2,3 +2,8 @@
 (defvar secrets-file (concat user-emacs-directory "env/secrets.el"))
 (defvar p-font-size "14")
 (defvar p-font (concat "FiraCode Nerd Font Mono-" p-font-size))
+
+(add-to-list 'exec-path "/opt/homebrew/bin/")
+(add-to-list 'exec-path "/opt/homebrew/sbin/")
+(setq eshell-path-env (mapconcat #'identity exec-path ":"))
+(setenv "PATH" eshell-path-env)
