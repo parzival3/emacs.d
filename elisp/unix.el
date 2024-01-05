9a12b76af8f96fdfc37f88faa3646d12f2686d9a
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2024-01-05 10:29:19 +0100

    elisp/unix: add compilation command for wsl and Windows

--- elisp/unix.el
+++ elisp/unix.el
@@ -4,4 +4,24 @@
 (use-package vterm
   :straight t)
 
+;; TODO: This should probably live in the wls config and not here
+(use-package project
+  :bind (:map project-prefix-map
+              ("c" . et-project-compile))
+
+  :config
+  (defun et-project-compile (arg)
+  "Run `compile' in the project root."
+  (interactive "P")
+  (if arg
+      (progn
+        (message "Running on Windows directory")
+        (let* ((project-dir (string-replace "~/Git/"  "/mnt/c/Git/" (project-root (project-current t))))
+               (default-directory project-dir)
+               (compilation-buffer-name-function
+                       (or project-compilation-buffer-name-function
+                           compilation-buffer-name-function)))
+          (call-interactively #'compile)))
+      (call-interactively #'project-compile))))
+
 (provide 'unix)
