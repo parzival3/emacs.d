5612685c893e84bd7420235f25164ba089a3e6e6
Enrico Tolotto <etolotto@gmail.com>
2024-01-13 05:35:55 +0100

    init: add artist mode config

--- init.el
+++ init.el
@@ -66,6 +66,36 @@
    'grep-find-command
    '("rg -n -H --no-heading -e '' $(git rev-parse --show-toplevel || pwd)" . 27)))
 
+(use-package artist
+  :bind
+  (:map artist-mode-map ("C-c C-a C-o" . 'et-select-artist-operation)
+                        ("C-c C-a C-c" . 'et-select-artist-settings))
+  :config
+     (defun et-select-artist-operation (type)
+     "Use ido to select a drawing operation in artist-mode"
+     (interactive (list (completing-read "Drawing operation: "
+                                             (list "Pen" "Pen Line" "line" "straight line" "rectangle"
+                                                   "square" "poly-line" "straight poly-line" "ellipse"
+                                                   "circle" "text see-thru" "text-overwrite" "spray-can"
+                                                   "erase char" "erase rectangle" "vaporize line" "vaporize lines"
+                                                   "cut rectangle" "cut square" "copy rectangle" "copy square"
+                                                   "paste" "flood-fill"))))
+     (artist-select-operation type))
+     (defun et-select-artist-settings (type)
+     "Use ido to select a setting to change in artist-mode"
+     (interactive (list (completing-read "Setting: "
+                                             (list "Set Fill" "Set Line" "Set Erase" "Spray-size" "Spray-chars"
+                                                   "Rubber-banding" "Trimming" "Borders"))))
+     (if (equal type "Spray-size")
+       (artist-select-operation "spray set size")
+       (call-interactively (artist-fc-get-fn-from-symbol
+			    (cdr (assoc type '(("Set Fill" . set-fill)
+					       ("Set Line" . set-line)
+					       ("Set Erase" . set-erase)
+					       ("Rubber-banding" . rubber-band)
+					       ("Trimming" . trimming)
+					       ("Borders" . borders)
+					       ("Spray-chars" . spray-chars)))))))))
 
 (use-package debugger
   :defer t
