a9b1cbbb6609a80f19cae5711e76053c5cb964cf
Enrico Tolotto <etolotto@gmail.com>
2024-01-14 07:16:33 +0100

    env/DK-C-BLR-ENTO2: fix git directory

--- env/DK-C-BLR-ENTO2.el
+++ env/DK-C-BLR-ENTO2.el
@@ -2,10 +2,13 @@
 (defvar secrets-file (concat user-emacs-directory "env/secrets.el"))
 
 (defvar et-font-size "12")
-(cond ((eq system-type 'gnu/linux)
-       (defvar et-font (concat "FiraCode-" et-font-size)))
+(cond
+      ((eq system-type 'gnu/linux)
+       (defvar et-font (concat "Fira Code-" et-font-size))
+       (defvar git-directory "~/Git/"))
       ((eq system-type 'windows-nt)
-       (defvar et-font (concat "FiraCode NFM-" et-font-size)))
+       (defvar et-font (concat "FiraCode NFM-" et-font-size))
+       (defvar git-directory "C:/Git/"))
       (t (error "Wrong system type")))
 
 (defvar et-theme 'doom-badger)
