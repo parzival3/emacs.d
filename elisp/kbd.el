7604618433ad798bcee7467ce65a00cdb07ac2d9
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2024-01-17 07:56:16 +0100

    elisp/kbd: update faces for meow cursor

--- elisp/kbd.el
+++ elisp/kbd.el
@@ -24,13 +24,16 @@ tab-indent."
   :ensure t
   :config
 
-  (set-face 'meow-normal-cursor 'nano-face-header-critical)
-  (set-face 'meow-beacon-cursor 'nano-face-header-critical)
-  (set-face 'meow-insert-cursor 'nano-face-header-critical)
-  (set-face 'meow-keypad-cursor 'nano-face-header-critical)
-  (set-face 'meow-motion-cursor 'nano-face-header-critical)
-  (set-face 'meow-kmacro-cursor 'nano-face-header-critical)
-  (set-face 'meow-unknown-cursor 'nano-face-header-critical)
+  (set-face 'meow-normal-cursor 'nano-face-default)
+  (set-face 'meow-beacon-cursor 'nano-face-default)
+  (set-face 'meow-insert-cursor 'nano-face-default)
+  (set-face 'meow-keypad-cursor 'nano-face-default)
+  (set-face 'meow-motion-cursor 'nano-face-default)
+  (set-face 'meow-kmacro-cursor 'nano-face-default)
+  (set-face 'meow-unknown-cursor 'nano-face-default)
+  ;; (set-face 'meow-position-highlight-number 'nano-face-salient)
+  ;; (set-face 'meow-search-indicator 'nano-face-tag-salient)
+  ;; (set-face 'meow-region-cursor-1 'nano-face-tag-salient)
 
   (defun et-is-current-coding-system (coding-system)
     (let ((eol-type-memonic (coding-system-eol-type-mnemonic buffer-file-coding-system)))
