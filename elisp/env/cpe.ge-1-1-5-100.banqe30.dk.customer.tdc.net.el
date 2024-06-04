4f40034619b094f4c2d57b3ab3b8d1d084366c3a
Enrico Tolotto (ENTO) <etolotto@gmail.com>
2024-06-04 09:36:08 +0200

    elisp/env/work-mac: update config

--- elisp/env/cpe.ge-1-1-5-100.banqe30.dk.customer.tdc.net.el
+++ elisp/env/cpe.ge-1-1-5-100.banqe30.dk.customer.tdc.net.el
@@ -17,3 +17,6 @@
 (if (not (display-graphic-p))
     (setq et-theme 'modus-vivendi)
   (setq et-theme 'doom-laserwave))
+
+(setq exec-path (append exec-path '("/Users/ento/tools")))
+(setenv "PATH" (concat (getenv "PATH") ":/Users/ento/tools"))
