a4c203a862ec0bc22bbc67c7efac887209c7375b
Enrico Tolotto (ENTO) <etolotto@gmail.com>
2024-09-20 09:25:14 +0200

    elisp/env/DK-C-BLR-MBJG-M: add guile under brew

--- elisp/env/DK-C-BLR-MBJG-M.el
+++ elisp/env/DK-C-BLR-MBJG-M.el
@@ -21,5 +21,11 @@
 (setq exec-path (append exec-path '("/Users/ento/tools")))
 (setenv "PATH" (concat (getenv "PATH") ":/Users/ento/tools"))
 
+;; Rust with brew
 (setenv "RUSTUP_HOME" (concat (getenv "HOME") "/development/rust/rustp"))
 (setenv "CARGO_HOME"  (concat (getenv "HOME") "/development/rust/cargo"))
+
+;; Guile with brew
+(setenv "GUILE_LOAD_PATH" "/opt/homebrew/share/guile/site/3.0")
+(setenv "GUILE_LOAD_COMPILED_PATH" "/opt/homebrew/lib/guile/3.0/site-ccache")
+(setenv "GUILE_SYSTEM_EXTENSIONS_PATH" "/opt/homebrew/lib/guile/3.0/extensions")
