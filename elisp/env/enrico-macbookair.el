0ec8977d786ec09fc265be699904b4370ace4dae
Enrico Tolotto <etolotto@gmail.com>
2024-09-20 20:14:56 +0200

    elisp/env/enrico-macbookair: fix font on macbook

--- elisp/env/enrico-macbookair.el
+++ elisp/env/enrico-macbookair.el
@@ -2,7 +2,9 @@
 (defvar et-font-size "12")
 (defvar et-font (concat "FiraCode Nerd Font Mono-" et-font-size))
 
-
 (if (not (display-graphic-p))
     (setq et-theme 'modus-vivendi)
   (setq et-theme 'doom-laserwave))
+
+
+(add-to-list 'default-frame-alist '(font . "Roboto Mono:style=Regular:size=28"))
