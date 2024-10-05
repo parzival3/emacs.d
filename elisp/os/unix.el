bc04f802fd3eededc9771445c75389387e1161a3
Enrico Tolotto <etolotto@gmail.com>
2024-10-05 11:31:26 +0200

    elisp/os/unix: fix geiser debug

--- elisp/os/unix.el
+++ elisp/os/unix.el
@@ -33,7 +33,7 @@
   :ensure nil
   :config
   ;; I can't use :bind here since it runs a compile time
-  (eval-and-compile `(bind-keys :map geiser-debug-mode-map ,@meow-normal-movement)))
+  (eval `(bind-keys :map geiser-debug-mode-map ,@meow-normal-movement)))
 
   ;; :init
   ;; (with-eval-after-load 'geiser-guile
