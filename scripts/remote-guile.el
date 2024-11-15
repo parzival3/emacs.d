aa9d4df609871bfcecd910503dece3212e99b6ea
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2024-11-15 15:31:11 +0100

    scripts/remote-guile: update scripts

---
+++ scripts/remote-guile.el
@@ -0,0 +1,30 @@
+(setq-local windows-guile-buffer-name "Windows Guile RPL")
+(setq-local geiser-repl-buffer-name-function (lambda (_) windows-guile-buffer-name))
+
+(setq-local windows-guile-buffer (or (get-buffer windows-guile-buffer-name)
+                                  (geiser-connect 'guile "localhost" 9919)))
+
+
+(defvar change-directory-to-dci
+  (prin1-to-string '(chdir "/c/Git/dci")))
+
+(defun run-system-command (command)
+  (prin1-to-string `(system ,command)))
+
+(defun set-current-branch ()
+  (run-system-command
+    (format "git fetch --all; git reset --hard origin/%s" (magit-get-current-branch))))
+
+(defun set-master-branch ()
+  (run-system-command "git reset --hard origin/master"))
+
+;; (with-current-buffer windows-guile-buffer
+;;   (geiser-repl--send change-directory-to-dci)
+;;   (geiser-repl--send (set-current-branch))
+;;   (geiser-repl--send (run-system-command "msbuild.exe -p:Configuration=Release kowalski/src"))
+;;   (geiser-repl--send (run-system-command "msbuild.exe -p:Configuration=Release skipperlite/src")))
+
+(with-current-buffer windows-guile-buffer
+  (geiser-repl--send change-directory-to-dci)
+  (geiser-repl--send (set-master-branch))
+  (geiser-repl--send (run-system-command "msbuild.exe -p:Configuration=Release skipperlite/src")))
---
+++ scripts/run_windows_guile.ps1
@@ -0,0 +1,14 @@
+# Create a shortcut to this file and place it on the desktop
+# C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "C:\Users\ento\.emacs.d\scripts\run_msys2_emacs.ps1"
+
+$vsinst = Get-VSSetupInstance
+$instpath = $vsinst[0].InstallationPath + "\\Common7\\Tools\Microsoft.VisualStudio.DevShell.dll"
+
+Import-Module $instpath
+Enter-VsDevShell -DevCmdArguments " -no_logo" $vsinst[0].InstanceId
+$env:MSYSTEM="MINGW64"
+$env:MSYS="winsymlinks:nativestrict"
+$env:MSYS2_PATH_TYPE="inherit"
+$env:PLATFORM="x64"
+
+C:/msys64/usr/bin/bash.exe --login -c "guile --listen=9919"
