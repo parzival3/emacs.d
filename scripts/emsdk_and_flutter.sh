00374a56842996e466cea6fe73db0919e2541576
Enrico Tolotto <etolotto@gmail.com>
2024-12-04 09:38:29 +0100

    manifest: update script

--- scripts/emsdk_and_flutter.sh
+++ scripts/emsdk_and_flutter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # To build the linux version I had to do this otherwise limis.h was not found
 # CPLUS_INCLUDE_PATH="/gnu/store/l9467jv5sw5cc2a60nl6w96sxs8ls5m2-linux-libre-headers-5.15.49/include" flutter build linux -v
 
