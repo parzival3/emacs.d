2b6480584da6f63fed4fd9f2e05e6130f22456f9
Enrico Tolotto <etolotto@gmail.com>
2024-12-10 09:15:57 +0100

    elisp/kbd-p-config: use wl-copy

--- elisp/kbd-p-config.el
+++ elisp/kbd-p-config.el
@@ -175,4 +175,24 @@
     (meow-setup)
     (meow-global-mode 1))
 
+
+(when wsl
+  ;; Setup wl-copytools for wls clipboard integration
+  ;; https://gist.github.com/yorickvP/6132f237fbc289a45c808d8d75e0e1fb
+
+  (setq wl-copy-process nil)
+  (defun wl-copy (text)
+    (setq wl-copy-process (make-process :name "wl-copy"
+                          :buffer nil
+                          :command '("wl-copy" "-f" "-n")
+                          :connection-type 'pipe))
+    (process-send-string wl-copy-process text)
+    (process-send-eof wl-copy-process))
+  (defun wl-paste ()
+    (if (and wl-copy-process (process-live-p wl-copy-process))
+      nil ; should return nil if we're the current paste owner
+      (shell-command-to-string "wl-paste -n | tr -d \r")))
+  (setq interprogram-cut-function 'wl-copy)
+  (setq interprogram-paste-function 'wl-paste))
+
 (provide 'kbd)
