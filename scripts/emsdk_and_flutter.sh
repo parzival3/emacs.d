da4ded723924f5f5ab7418792c3186e232246408
Enrico Tolotto (ENTO) <ento@eposaudio.com>
2025-01-13 14:26:40 +0100

    scripts/emsdk_and_flutter: update dependencies

--- scripts/emsdk_and_flutter.sh
+++ scripts/emsdk_and_flutter.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # To build the linux version I had to do this otherwise limis.h was not found
 # CPLUS_INCLUDE_PATH="/gnu/store/l9467jv5sw5cc2a60nl6w96sxs8ls5m2-linux-libre-headers-5.15.49/include" flutter build linux -v
 
@@ -53,4 +53,6 @@ exec guix shell -C --pure  -F -N -E ^DISPLAY -E ^XDG_RUNTIME_DIR --share=/home/e
   "cmake"
   "ninja"
   "eudev"
+  "go"
+  "zip"
   "gtk+"))
